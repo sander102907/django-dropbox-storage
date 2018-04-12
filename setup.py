e8e0aa2e2351ce0b2e4e09dad9b63a5a609a7664
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2018-04-12 15:08:02 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
-import os
-from setuptools import setup
+from os import path
+from codecs import open
+from setuptools import setup, find_packages
 from django_dropbox_storage import version
 
-
-def get_packages():
-    # setuptools can't do the job :(
-    packages = []
-    for root, dirnames, filenames in os.walk('django_dropbox_storage'):
-        if '__init__.py' in filenames:
-            packages.append(".".join(os.path.split(root)).strip("."))
-
-    return packages
+packages = find_packages(exclude=['contrib', 'docs', 'tests'])
 
 requires = [
     'django>=1.11',
@@ -20,15 +13,23 @@ requires = [
 
 project_url = 'https://github.com/zuck/django-dropbox-storage'
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='django-dropbox-storage',
     version=version,
     description='A Dropbox Storage for your Django apps',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author=u'Emanuele Bertoldi',
     author_email='emanuele.bertoldi@gmail.com',
     url=project_url,
     download_url=project_url + '/archive/' + version + '.tar.gz',
-    packages=get_packages(),
+    packages=packages,
     install_requires=requires,
     keywords = 'django storage dropbox',
     license='MIT',
